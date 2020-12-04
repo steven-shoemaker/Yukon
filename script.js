4baa15cd7f342bc05a4c6f9198b6c9cbbbd6c2f6
Steven Shoemaker <75040735+steven-shoemaker@users.noreply.github.com>
2020-12-04 11:34:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 require([
         "esri/WebMap",
         "esri/views/MapView",
+        "esri/widgets/LayerList",
         "esri/widgets/Search"
-      ], function (WebMap, MapView,LayerList, Search) {
+      ], function (WebMap, MapView, LayerList, Search) {
         const map = new WebMap({
           portalItem: {
             id: "7df1ffb5001b4c7e87c08f1e3b381a9d"
@@ -14,3 +15,36 @@ require([
           container: "viewDiv",
           map: map
         });
+
+        // Add a legend instance to the panel of a
+        // ListItem in a LayerList instance
+        const layerList = new LayerList({
+          view: view,
+          listItemCreatedFunction: function (event) {
+            const item = event.item;
+            if (item.layer.type != "group") {
+              // don't show legend twice
+              item.panel = {
+                content: "legend",
+                open: false
+              };
+            }
+          }
+        });
+        view.ui.add(layerList, "top-right");
+      });
+
+var legend = new Legend({
+  view: view,
+  layerInfos: [{
+    layer: featureLayer,
+    title: "Legend"
+  }]
+});
+
+view.ui.add(legend, "bottom-right");
+
+         var search = new Search({
+            map: ma
+         }, "search");
+         search.startup();
